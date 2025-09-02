e7516362e8f470f5fe5a274719d217ea8a815ec9
William Huynh <whuynh23@gmail.com>
2025-09-02 22:07:39 +0100

    small screen width email form

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { 
   Box,
   Button,
@@ -11,34 +11,36 @@ import { Send } from '@mui/icons-material';
 
 export const EmailForm = () => {
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const textFieldWidth = buttonRef.current ? `calc(100% - ${buttonRef.current.offsetWidth}px - 8px)` : '80%';
+
   return (
     <Box
       sx={{
-        // textAlign: 'right'
-
+        'textAlign': 'right'
       }}
     >
       <TextField
         size="small"
         label="Email"
         color="default"
-        // fullWidth
         sx={{
           height: 40,
-          width: 'calc(80% - 8px)',
-          marginRight: '8px'
+          width: textFieldWidth,
+          marginRight: '8px',
         }}
       />
-       <Button
-          variant="outlined"
-          color="default"
-          sx={{
-            height: 40,
-            width: '20%'
-          }}
-        >
-          <Send />
-        </Button>
+      <Button
+        ref={buttonRef}
+        variant="outlined"
+        color="default"
+        sx={{
+          height: 40,
+          width: 'calc(20% - 8px)',
+        }}
+      >
+        <Send />
+      </Button>
    
     </Box>
   );
